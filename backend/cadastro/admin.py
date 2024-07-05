fba0fa6d790ef17e347d0c53225dc64a3478eb13
Adriano Cesar <cesar.adriano087@gmail.com>
2024-07-05 17:03:39 -0300

    update 05.07.2024

--- backend/cadastro/admin.py
+++ backend/cadastro/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Produtos_Frasson, Detalhamento_Servicos, Cadastro_Pessoal
+from .models import Produtos_Frasson, Detalhamento_Servicos, Cadastro_Pessoal, Categoria_Cadastro
+from .models import Instituicoes_Razao_Social, Instituicoes_Parceiras, Tipo_Instituicao
 
 @admin.register(Produtos_Frasson)
 class Produtos_FrassonAdmin(admin.ModelAdmin):
@@ -12,3 +13,19 @@ class Detalhamento_ServicosAdmin(admin.ModelAdmin):
 @admin.register(Cadastro_Pessoal)
 class Cadastro_PessoalAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'razao_social',)
+
+@admin.register(Categoria_Cadastro)
+class Categoria_CadastroAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'categoria',)
+
+@admin.register(Tipo_Instituicao)
+class Tipo_InstituicaoAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'descricao')
+
+@admin.register(Instituicoes_Razao_Social)
+class Instituicoes_Razao_SocialAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'tipo', 'razao_social', 'cnpj')
+
+@admin.register(Instituicoes_Parceiras)
+class Instituicoes_ParceirasAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'instituicao', 'identificacao')
